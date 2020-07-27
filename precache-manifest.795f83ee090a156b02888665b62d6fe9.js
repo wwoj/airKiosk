3f572082b14d99ae9cf30f92e90b140284557809
wwoj <wojcik.wojtek@yahoo.pl>
2020-07-28 00:01:55 +0200

    Updates

---
+++ precache-manifest.795f83ee090a156b02888665b62d6fe9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07de26714e04c0d94502f1dc5ffb6084",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb12e03d18eabeb6abb4",
+    "url": "/static/css/2.1d143842.chunk.css"
+  },
+  {
+    "revision": "6dcf76a1c695f6b374e0",
+    "url": "/static/css/main.77922d62.chunk.css"
+  },
+  {
+    "revision": "bb12e03d18eabeb6abb4",
+    "url": "/static/js/2.ec708c45.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ec708c45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dcf76a1c695f6b374e0",
+    "url": "/static/js/main.de47ce29.chunk.js"
+  },
+  {
+    "revision": "200f6f9d61d2bf33a3d2",
+    "url": "/static/js/runtime-main.68712f13.js"
+  },
+  {
+    "revision": "c77b7615eb7b1bf47f4297cba5fddb54",
+    "url": "/static/media/LOT_Icon.c77b7615.png"
+  },
+  {
+    "revision": "39bf4b3f0e5bb44cc687ab641b2303cb",
+    "url": "/static/media/carousel_0.39bf4b3f.jpg"
+  },
+  {
+    "revision": "e3f6cf542ed33e757e275f0f6fb6f93a",
+    "url": "/static/media/carousel_2.e3f6cf54.jpg"
+  },
+  {
+    "revision": "c4267d8921f9f0474063a31fc53b5bf4",
+    "url": "/static/media/carousel_4.c4267d89.JPG"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
